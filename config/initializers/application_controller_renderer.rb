3d9ac1f8591bbfc65971593ba5d1b5166f65a84c
zaptot <zaptot@gmail.com>
2021-01-27 13:43:39 +0000

    test default uri options

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+Rails.application.routes.default_url_options[:host] = 'youtrack-to-jira-migrator.softswiss.net'
