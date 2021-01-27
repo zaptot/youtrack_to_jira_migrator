f8050265bc68c63e4b47c9be83489d6b1dd2a389
zaptot <zaptot@gmail.com>
2021-01-27 14:03:53 +0000

    fix init

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,1 +1,8 @@
-Rails.application.routes.default_url_options[:host] = 'youtrack-to-jira-migrator.softswiss.net'
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
