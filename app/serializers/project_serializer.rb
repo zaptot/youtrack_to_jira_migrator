eb58644f369b2f67f31cd56a4754223305eede00
dzmitry.mikulich <dzmitry.mikulich@softswiss.com>
2022-02-16 18:31:05 +0300

    fix versions list

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ProjectSerializer < ActiveModel::Serializer
+  VERSION_FIELDS = ['Fix versions', 'Verified in Version', 'Found in'].freeze
+
   attributes :type, :versions, full_name: :name, id: :key, workflow_name: :workflowSchemeName
 
   has_many :issues
@@ -10,6 +12,6 @@ class ProjectSerializer < ActiveModel::Serializer
   end
 
   def versions
-    (object.issues.system_field_values('Fix versions') + object.issues.system_field_values('Verified in Version')).uniq
+    VERSION_FIELDS.inject([]) { |memo, field| memo += object.issues.system_field_values(field) }.sort.uniq
   end
 end
